70499491f41f95f4be3c40729c3854a8f5a901ee
d22tny <d22tny@gmail.com>
2021-11-19 01:26:58 +0200

    if 4 fisiere bune

--- Tema 1/if4.c
+++ Tema 1/if4.c
@@ -4,12 +4,12 @@ int main() {
     char ch_arr[52][13] = {"Andrei","Albert","Bogdan","Bianca","Cornel","Constantin","Dragos","Dinu","Emanuel","Emil","Florin","Florentina","George","Georgiana","Horia","Horatiu","Ion","Ioana","Jean","Jan","Karina","Kevin","Laurentiu","Lucian","Matei","Marius","Nicolae","Nicoleta","Oana","Onel","Pavel","Petre","Quinn","Quinto","Rares","Raul","Sebastian","Sorin","Tony","Tudor","Umar","Uriel","Vali","Viorica","Wilson","Wilmark","Xander","Xenia","Yvonne","Yanis","Zoran","Zac"};
     char litera;
     FILE *f,*g;
-    if(!(f=fopen("numar.in.txt","r")))
+    if(!(f=fopen("litera.in.txt","r")))
         printf ("Eroare la deschiderea fisierului text!");
     else {
         fscanf(f,"%c",&litera);
         fclose(f);
-        if(!(g=fopen("numar.out.txt","w")))
+        if(!(g=fopen("nume.out.txt","w")))
             printf ("Eroare la deschiderea fisierului text!");
         else {
             for (int i = 0;i<=51;i++) {
