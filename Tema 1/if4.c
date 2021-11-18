cadf28adc7bb001d38946a9fe4d6b875b1a3c0f5
d22tny <d22tny@gmail.com>
2021-11-19 00:40:23 +0200

    if 4

--- Tema 1/if4.c
+++ Tema 1/if4.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 int main() {
-    char a = 'a';
-    char b = 'A';
-
-    char ch_arr[51][13] = {"Andrei","Albert","Bogdan","Bianca","Cornel","Constantin","Dragos","Dinu","Emanuel","Emil","Florin","Florentina","George","Georgiana","Horia","Horatiu","Ion","Ioana","Jean","Jan","Karina","Kevin","Laurentiu","Lucian","Matei","Marius","Nicolae","Nicoleta","Oana","Onel","Pavel","Petre","Quinn","Quinto","Rares","Raul","Sebastian","Sorin","Tony","Tudor","Umar","Uriel","Vali","Viorica","Wilson","Wilmark","Xander","Xenia","Yvonne","Yanis","Zoran","Zac"};
+    char ch_arr[52][13] = {"Andrei","Albert","Bogdan","Bianca","Cornel","Constantin","Dragos","Dinu","Emanuel","Emil","Florin","Florentina","George","Georgiana","Horia","Horatiu","Ion","Ioana","Jean","Jan","Karina","Kevin","Laurentiu","Lucian","Matei","Marius","Nicolae","Nicoleta","Oana","Onel","Pavel","Petre","Quinn","Quinto","Rares","Raul","Sebastian","Sorin","Tony","Tudor","Umar","Uriel","Vali","Viorica","Wilson","Wilmark","Xander","Xenia","Yvonne","Yanis","Zoran","Zac"};
     char litera;
     FILE *f,*g;
     if(!(f=fopen("numar.in.txt","r")))
@@ -15,7 +12,7 @@ int main() {
         if(!(g=fopen("numar.out.txt","w")))
             printf ("Eroare la deschiderea fisierului text!");
         else {
-            for (int i = 0;i<=50;i++) {
+            for (int i = 0;i<=51;i++) {
                 if (tolower(ch_arr[i][0]) == tolower(litera)) {
                     fprintf(f,"%s\n", ch_arr[i]);
                 }
